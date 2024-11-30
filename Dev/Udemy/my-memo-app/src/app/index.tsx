9f0f1ca01644dd873d6e9c54704e81205ba222a2
hinatao <189737370+hinamomo0306@users.noreply.github.com>
2024-11-30 22:44:36 +0900

    57.メモの一覧画面をマークアップ

--- Dev/Udemy/my-memo-app/src/app/index.tsx
+++ Dev/Udemy/my-memo-app/src/app/index.tsx
@@ -1,10 +1,76 @@
-import { View, Text } from "react-native"
+import { View, Text, StyleSheet } from "react-native"
 
 const Index = (): JSX.Element => {
   return(
-    <View>
-      <Text>Hello World</Text>
+    // 一番外枠
+    <View style={styles.container}>
+      <View>
+        <View>
+          <Text>Memo App</Text>
+          <Text>Log Out</Text>
+        </View>
+      </View>
+      <View>
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年11月30日 22:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年11月30日 22:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年11月30日 22:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2024年11月30日 22:00</Text>
+          </View>
+          <View>
+            <Text>X</Text>
+          </View>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>+</Text>
+      </View>
     </View>
+    // 一番外枠
   )
 }
+
+const styles = StyleSheet.create ({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center"
+  }
+})
+
 export default Index
