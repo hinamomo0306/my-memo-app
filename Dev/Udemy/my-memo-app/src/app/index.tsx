307d6337d0f5e8e9ab22e8fd20a17dd749d8f66a
hinatao <189737370+hinamomo0306@users.noreply.github.com>
2024-12-01 23:45:07 +0900

    62.リダイレクト設定

--- Dev/Udemy/my-memo-app/src/app/index.tsx
+++ Dev/Udemy/my-memo-app/src/app/index.tsx
@@ -1,35 +1,7 @@
-import { View, StyleSheet } from "react-native"
+import { Redirect } from "expo-router"
 
-import Header from "../components/header"
-import MemoListItem from "../components/memoListItem"
-import CircleButton from "../components/circleButton"
-
-const Index = (): JSX.Element => {
-  return(
-    // 一番外枠
-    <View style={styles.container}>
-      <Header />
-      <View>
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-        <MemoListItem />
-      </View>
-      <CircleButton>+</CircleButton>
-    </View>
-    // 一番外枠
-  )
+const Index = () => {
+  return <Redirect href={"memo/list"} />
 }
-
-const styles = StyleSheet.create ({
-  container: {
-    flex: 1,
-    backgroundColor: "#f5f8fa"
-  }
-})
 
 export default Index
